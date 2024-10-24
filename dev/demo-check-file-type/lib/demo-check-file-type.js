b89d6d406911549ef8b9a450507deaa751e2443d
loveencounterflow <flow@home>
2024-10-24 20:57:03 +0200

    update

--- dev/demo-check-file-type/lib/demo-check-file-type.js
+++ dev/demo-check-file-type/lib/demo-check-file-type.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-  var FS, GUY, PATH, alert, debug, dsc, echo, filename, filenames, get_file_descriptor, help, i, info, inspect, len, log, path, plain, praise, reverse, rpr, types_and_methods, urge, warn, whisper;
+  var FS, GUY, PATH, _get_short_file_descriptor, alert, debug, dsc, echo, filename, filenames, get_long_file_descriptor, get_short_file_descriptor, help, i, info, inspect, len, log, path, plain, praise, reverse, rpr, types_and_methods, urge, warn, whisper;
 
   //===========================================================================================================
   GUY = require('guy');
@@ -13,7 +13,7 @@
 
   PATH = require('node:path');
 
-  filenames = ['isa-nosuch', 'isa-circular-symlink', 'isa-file', 'isa-file-symlink', 'isa-folder', 'isa-folder-symlink', 'isa-nowhere-symlink'];
+  filenames = ['isa-nosuch', 'isa-nowhere-symlink', 'isa-circular-symlink', 'isa-file-symlink', 'isa-folder-symlink', 'isa-file', 'isa-folder'];
 
   types_and_methods = [
     {
@@ -43,8 +43,11 @@
   ];
 
   //-----------------------------------------------------------------------------------------------------------
-  get_file_descriptor = function(path) {
+  _get_short_file_descriptor = function(path) {
     var is_loop, link, lstat, stat, type;
+    type = null;
+    link = false;
+    is_loop = false;
     //.........................................................................................................
     [lstat, link] = (() => {
       var error;
@@ -61,9 +64,8 @@
     })();
     if (lstat == null) {
       //.........................................................................................................
-      return null;
+      return {type, link, is_loop};
     }
-    info('Ω___1', PATH.basename(path), lstat != null, link);
     //.........................................................................................................
     if (link) {
       [stat, is_loop] = (() => {
@@ -83,17 +85,14 @@
         return [stat, false];
       })();
     }
-    debug('Ω___2', (PATH.basename(path)).padEnd(20), {
-      lstat: lstat != null,
-      stat: stat != null,
-      link,
-      is_loop
-    });
+    if (is_loop == null) {
+      is_loop = false;
+    }
     if (is_loop || (!link)) {
       stat = lstat;
     }
     if ((stat == null) && (!is_loop)) {
-      return null;
+      return {type, link, is_loop};
     }
     type = (() => {
       var i, len, method_name;
@@ -105,19 +104,49 @@
       }
       return null;
     })();
-    return {
-      //.........................................................................................................
-      type,
-      link,
-      is_loop: false
-    };
+    //.........................................................................................................
+    return {type, link, is_loop};
   };
 
+  //-----------------------------------------------------------------------------------------------------------
+  get_short_file_descriptor = function(...P) {
+    return _get_short_file_descriptor(...P);
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  get_long_file_descriptor = function(...P) {
+    var dsc, stats;
+    throw new Error("not implemented");
+    return ({dsc, stats} = _get_short_file_descriptor(...P));
+  };
+
+// dev: 2114n,
+// ino: 48064969n,
+// mode: 33188n,
+// nlink: 1n,
+// uid: 85n,
+// gid: 100n,
+// rdev: 0n,
+// size: 527n,
+// blksize: 4096n,
+// blocks: 8n,
+// atimeMs: 1318289051000n,
+// mtimeMs: 1318289051000n,
+// ctimeMs: 1318289051000n,
+// birthtimeMs: 1318289051000n,
+// atimeNs: 1318289051000000000n,
+// mtimeNs: 1318289051000000000n,
+// ctimeNs: 1318289051000000000n,
+// birthtimeNs: 1318289051000000000n,
+// atime: Mon, 10 Oct 2011 23:24:11 GMT,
+// mtime: Mon, 10 Oct 2011 23:24:11 GMT,
+// ctime: Mon, 10 Oct 2011 23:24:11 GMT,
+// birthtime: Mon, 10 Oct 2011 23:24:11 GMT
   for (i = 0, len = filenames.length; i < len; i++) {
     filename = filenames[i];
-    help();
+    // help()
     path = PATH.resolve(__dirname, PATH.join('../probes', filename));
-    dsc = get_file_descriptor(path);
+    dsc = get_short_file_descriptor(path);
     help('Ω___4', filename.padEnd(20), dsc);
   }
 
--- dev/demo-check-file-type/src/demo-check-file-type.coffee
+++ dev/demo-check-file-type/src/demo-check-file-type.coffee
@@ -22,12 +22,13 @@ PATH                      = require 'node:path'
 
 filenames = [
   'isa-nosuch'
+  'isa-nowhere-symlink'
   'isa-circular-symlink'
-  'isa-file'
   'isa-file-symlink'
-  'isa-folder'
   'isa-folder-symlink'
-  'isa-nowhere-symlink' ]
+  'isa-file'
+  'isa-folder'
+  ]
 
 types_and_methods = [
   { type: 'file',       method_name: 'isFile',            }
@@ -39,7 +40,10 @@ types_and_methods = [
 ]
 
 #-----------------------------------------------------------------------------------------------------------
-get_file_descriptor = ( path ) ->
+_get_short_file_descriptor = ( path ) ->
+  type    = null
+  link    = false
+  is_loop = false
   #.........................................................................................................
   [ lstat, link, ] = do =>
     try lstat = FS.lstatSync path catch error
@@ -47,8 +51,7 @@ get_file_descriptor = ( path ) ->
       throw error
     return [ lstat, lstat.isSymbolicLink(), ]
   #.........................................................................................................
-  return null unless lstat?
-  info 'Ω___1', ( PATH.basename path ), lstat?, link
+  return { type, link, is_loop, } unless lstat?
   #.........................................................................................................
   if link then [ stat, is_loop, ] = do =>
     try stat = FS.statSync path catch error
@@ -56,20 +59,51 @@ get_file_descriptor = ( path ) ->
       return [ null,  false,  ] if error.code is 'ENOENT'
       throw error
     return [ stat, false, ]
-  debug 'Ω___2', ( ( PATH.basename path ).padEnd 20 ), { lstat: lstat?, stat: stat?, link, is_loop}
-  stat = lstat if is_loop or ( not link )
-  return null if ( not stat? ) and ( not is_loop )
+  is_loop  ?= false
+  stat      = lstat if is_loop or ( not link )
+  return { type, link, is_loop, } if ( not stat? ) and ( not is_loop )
   type = do =>
     for { type, method_name, } in types_and_methods
       return type if stat[ method_name ]()
     return null
   #.........................................................................................................
-  return { type, link, is_loop: false, }
+  return { type, link, is_loop, }
+
+#-----------------------------------------------------------------------------------------------------------
+get_short_file_descriptor = ( P... ) ->
+  _get_short_file_descriptor P...
+
+#-----------------------------------------------------------------------------------------------------------
+get_long_file_descriptor = ( P... ) ->
+  throw new Error "not implemented"
+  { dsc, stats } = _get_short_file_descriptor P...
+# dev: 2114n,
+# ino: 48064969n,
+# mode: 33188n,
+# nlink: 1n,
+# uid: 85n,
+# gid: 100n,
+# rdev: 0n,
+# size: 527n,
+# blksize: 4096n,
+# blocks: 8n,
+# atimeMs: 1318289051000n,
+# mtimeMs: 1318289051000n,
+# ctimeMs: 1318289051000n,
+# birthtimeMs: 1318289051000n,
+# atimeNs: 1318289051000000000n,
+# mtimeNs: 1318289051000000000n,
+# ctimeNs: 1318289051000000000n,
+# birthtimeNs: 1318289051000000000n,
+# atime: Mon, 10 Oct 2011 23:24:11 GMT,
+# mtime: Mon, 10 Oct 2011 23:24:11 GMT,
+# ctime: Mon, 10 Oct 2011 23:24:11 GMT,
+# birthtime: Mon, 10 Oct 2011 23:24:11 GMT
 
 for filename in filenames
-  help()
+  # help()
   path    = PATH.resolve __dirname, PATH.join '../probes', filename
-  dsc     = get_file_descriptor path
+  dsc     = get_short_file_descriptor path
   help 'Ω___4', ( filename.padEnd 20 ), dsc
 
 
