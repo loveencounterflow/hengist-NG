77f42cc7c46450d46cc78f5b0dc9f09f9162de78
loveencounterflow <flow@home>
2024-10-30 10:22:51 +0100

    update

--- dev/snippets/lib/demo-cli-interface.js
+++ dev/snippets/lib/demo-cli-interface.js
@@ -1,6 +1,6 @@
 (async function() {
   'use strict';
-  var CLK, Clack, GUY, Interactive_dialog, PATH, Programmatic_dialog, Programmatic_dialog_error, alert, bold, debug, demo_run_interactive, demo_run_programmatic, echo, help, info, inspect, log, mark, plain, praise, reverse, rpr, sample_dialog, urge, warn, whisper;
+  var CLK, Dialog_error, Dialog_failure, GUY, Interactive_dialog, Interactive_dialog_error, Internal_error, Internal_failure, Misstep_failure, Overrun_error, Overrun_failure, PATH, Programmatic_dialog, Programmatic_dialog_error, Underrun_failure, alert, bold, debug, demo_run_interactive, demo_run_programmatic, echo, help, info, inspect, log, mark, plain, praise, reverse, rpr, sample_dialog, urge, warn, whisper;
 
   //===========================================================================================================
   GUY = require('guy');
@@ -18,105 +18,97 @@
     return urge(reverse(bold(` ${ref} `)));
   };
 
+  // #===========================================================================================================
+  // class Clack
+
+    //   #---------------------------------------------------------------------------------------------------------
+  //   intro_outro: ->
+  //     CLK.intro "create-my-app"
+  //     value = await CLK.text { message: "press CTRL+C to continue", }
+  //     if CLK.isCancel value
+  //       CLK.cancel "Operation cancelled."
+  //       # process.exit 0
+  //     CLK.outro "You're all set!"
+  //     return null
+
+    //   #---------------------------------------------------------------------------------------------------------
+  //   text: ->
+  //     cfg =
+  //       message:      "What is your name?"
+  //       placeholder:  "Not sure"
+  //       initialValue: "Jim"
+  //       validate:     ( value ) ->
+  //         # debug 'Ω___1', rpr value
+  //         return "Value is required!" if value.length is 0
+  //         return null
+  //     name = await CLK.text cfg
+  //     info "Ω___2 your name is #{rpr name}"
+  //     return null
+
+    //   #---------------------------------------------------------------------------------------------------------
+  //   select: ->
+  //     await do =>
+  //       cfg =
+  //         message:    "Pick a project type."
+  //         options: [
+  //           { value: 'ts',      label: 'TypeScript' },
+  //           { value: 'js',      label: 'JavaScript' },
+  //           { value: 'coffee',  label: 'CoffeeScript', hint: 'yes!' }, ]
+  //       project_type = await CLK.select cfg
+  //       info "Ω___3 project type: #{rpr project_type}"
+  //       return null
+  //     await do =>
+  //       spinner = CLK.spinner()
+  //       spinner.start "asking questions"
+  //       cfg =
+  //         message:    "Select additional tools."
+  //         options: [
+  //           { value: 'eslint', label: 'ESLint', hint: 'recommended' },
+  //           { value: 'prettier', label: 'Prettier' },
+  //           { value: 'gh-action', label: 'GitHub Action' }, ]
+  //         required: false
+  //       tools = await CLK.multiselect cfg
+  //       info "Ω___4 tools: #{rpr tools}"
+  //       spinner.stop "thanks!"
+  //       return null
+  //     return null
+  // # clack = new Clack()
+  /*
+
+  Errors entail failures
+
+  */
   //===========================================================================================================
-  Clack = class Clack {
-    //---------------------------------------------------------------------------------------------------------
-    async intro_outro() {
-      var value;
-      CLK.intro("create-my-app");
-      value = (await CLK.text({
-        message: "press CTRL+C to continue"
-      }));
-      if (CLK.isCancel(value)) {
-        CLK.cancel("Operation cancelled.");
-      }
-      // process.exit 0
-      CLK.outro("You're all set!");
-      return null;
-    }
+  /* TAINT Later to be extended so we pass in parameters, not messages */
+  Dialog_error = class Dialog_error extends Error {};
 
-    //---------------------------------------------------------------------------------------------------------
-    async text() {
-      var cfg, name;
-      cfg = {
-        message: "What is your name?",
-        placeholder: "Not sure",
-        initialValue: "Jim",
-        validate: function(value) {
-          if (value.length === 0) {
-            // debug 'Ω___1', rpr value
-            return "Value is required!";
-          }
-          return null;
-        }
-      };
-      name = (await CLK.text(cfg));
-      info(`Ω___2 your name is ${rpr(name)}`);
-      return null;
-    }
+  Interactive_dialog_error = class Interactive_dialog_error extends Dialog_error {};
 
-    //---------------------------------------------------------------------------------------------------------
-    async select() {
-      await (async() => {
-        var cfg, project_type;
-        cfg = {
-          message: "Pick a project type.",
-          options: [
-            {
-              value: 'ts',
-              label: 'TypeScript'
-            },
-            {
-              value: 'js',
-              label: 'JavaScript'
-            },
-            {
-              value: 'coffee',
-              label: 'CoffeeScript',
-              hint: 'yes!'
-            }
-          ]
-        };
-        project_type = (await CLK.select(cfg));
-        info(`Ω___3 project type: ${rpr(project_type)}`);
-        return null;
-      })();
-      await (async() => {
-        var cfg, spinner, tools;
-        spinner = CLK.spinner();
-        spinner.start("asking questions");
-        cfg = {
-          message: "Select additional tools.",
-          options: [
-            {
-              value: 'eslint',
-              label: 'ESLint',
-              hint: 'recommended'
-            },
-            {
-              value: 'prettier',
-              label: 'Prettier'
-            },
-            {
-              value: 'gh-action',
-              label: 'GitHub Action'
-            }
-          ],
-          required: false
-        };
-        tools = (await CLK.multiselect(cfg));
-        info(`Ω___4 tools: ${rpr(tools)}`);
-        spinner.stop("thanks!");
-        return null;
-      })();
-      return null;
+  Programmatic_dialog_error = class Programmatic_dialog_error extends Dialog_error {};
+
+  Internal_error = class Internal_error extends Programmatic_dialog_error {};
+
+  Overrun_error = class Overrun_error extends Programmatic_dialog_error {};
+
+  //===========================================================================================================
+  /* TAINT Later to be extended so we pass in parameters, not messages */
+  Dialog_failure = class Dialog_failure {
+    constructor(message1) {
+      this.message = message1;
+      void 0;
     }
 
   };
 
-  // clack = new Clack()
+  Misstep_failure = class Misstep_failure extends Dialog_failure {};
 
-    //===========================================================================================================
+  Underrun_failure = class Underrun_failure extends Dialog_failure {};
+
+  Overrun_failure = class Overrun_failure extends Dialog_failure {};
+
+  Internal_failure = class Internal_failure extends Dialog_failure {};
+
+  //===========================================================================================================
   Interactive_dialog = class Interactive_dialog {
     //---------------------------------------------------------------------------------------------------------
     ctrlc(value) {
@@ -141,6 +133,10 @@
       return this.ctrlc((await CLK.confirm(cfg)));
     }
 
+    finish() {
+      return null;
+    }
+
     //---------------------------------------------------------------------------------------------------------
     process_exit(code = 0) {
       return process.exit(code);
@@ -149,38 +145,49 @@
   };
 
   //===========================================================================================================
-  Programmatic_dialog_error = class Programmatic_dialog_error extends Error {};
-
-  //===========================================================================================================
   Programmatic_dialog = class Programmatic_dialog {
     //---------------------------------------------------------------------------------------------------------
     constructor(steps) {
-      this.exp_steps = steps;
-      this.pc = -1;
-      this.act_steps = [];
-      this.errors = [];
+      this._exp_steps = steps;
+      this._pc = -1;
+      this._act_steps = [];
+      //.......................................................................................................
+      GUY.props.def(this, '_failures', {
+        enumerable: false,
+        configurable: false,
+        get: function() {
+          var d, i, len, ref1, results;
+          ref1 = this._act_steps;
+          results = [];
+          for (i = 0, len = ref1.length; i < len; i++) {
+            d = ref1[i];
+            if (d instanceof Dialog_failure) {
+              results.push(d);
+            }
+          }
+          return results;
+        }
+      });
+      //.......................................................................................................
       return void 0;
     }
 
     //---------------------------------------------------------------------------------------------------------
     _next() {
-      var R, ref1;
-      this.pc++;
-      if ((R = (ref1 = this.exp_steps[this.pc]) != null ? ref1 : null) == null) {
-        throw new Programmatic_dialog_error(`running out of steps: ${rpr(this)}`);
+      var R, message, ref1;
+      this._pc++;
+      if ((R = (ref1 = this._exp_steps[this._pc]) != null ? ref1 : null) == null) {
+        message = `emergency halt, running too long: act ${this._act_steps.length + 1} exp ${this._exp_steps.length}`;
+        this._fail(new Overrun_failure(message));
+        throw new Overrun_error(message);
       }
       return R;
     }
 
     //---------------------------------------------------------------------------------------------------------
-    _error(message) {
-      var error;
-      error = {
-        isa: 'error',
-        message
-      };
-      this.act_steps.push(error);
-      this.errors.push(error);
+    _fail(failure) {
+      this._act_steps.push(failure);
+      this._failures.push(failure);
       return null;
     }
 
@@ -189,11 +196,38 @@
       var exp_key, value;
       [exp_key, value] = this._next();
       if (act_key === exp_key) {
-        this.act_steps.push(act_key);
+        this._act_steps.push(act_key);
       } else {
-        this._error(`error @ ${this.pc}: act ${rpr(act_key)}, exp ${rpr(exp_key)}`);
+        this._act_steps.push(new Misstep_failure(`step#${this._pc}: act ${rpr(act_key)}, exp ${rpr(exp_key)}`));
       }
       return value;
+    }
+
+    //---------------------------------------------------------------------------------------------------------
+    _is_finished() {
+      return this._act_steps.length === this._exp_steps.length;
+    }
+
+    _is_underrun() {
+      return this._act_steps.length < this._exp_steps.length;
+    }
+
+    _is_overrun() {
+      return this._act_steps.length > this._exp_steps.length;
+    }
+
+    //---------------------------------------------------------------------------------------------------------
+    finish(...P) {
+      if (this._is_finished() || this._is_overrun()) {
+        //### `dlg.finish()` should be called after the simulated dialog has ben run to issue an  ####
+        return true;
+      }
+      // unless @_is_underrun()
+      //   message = "should have recognized overrun"
+      //   @_fail new Internal_failure message
+      //   throw new Internal_error message
+      this._fail(new Underrun_failure(`finished too early: act ${this._act_steps.length} exp ${this._exp_steps.length}`));
+      return false;
     }
 
     //---------------------------------------------------------------------------------------------------------
@@ -249,13 +283,12 @@
   demo_run_programmatic = async function() {
     var dlg, error, steps;
     steps = [
-      ['intro',
-      true],
+      ['intro'],
       ['confirm',
-      true],
-      // [ 'confirm',  false, ]
-      ['outro',
-      true]
+      false],
+      // [ 'confirm',  true, ]
+      ['outro'],
+      ['outro']
     ];
     dlg = new Programmatic_dialog(steps);
     try {
@@ -265,12 +298,11 @@
       if (!(error instanceof Programmatic_dialog_error)) {
         throw error;
       }
-      warn('Ω__14', reverse(bold(error.message)));
+      warn('Ω___7', reverse(bold(error.message)));
     }
-    if (dlg.errors.length > 0) {
-      warn('Ω__15', dlg.errors);
-    }
-    help('Ω__16', dlg.act_steps);
+    dlg.finish();
+    warn('Ω___9', dlg._failures);
+    help('Ω__10', dlg._act_steps);
     return null;
   };
 
--- dev/snippets/src/demo-cli-interface.coffee
+++ dev/snippets/src/demo-cli-interface.coffee
@@ -26,61 +26,88 @@ PATH                      = require 'node:path'
 mark                      = ( ref ) -> urge reverse bold " #{ref} "
 
 
+# #===========================================================================================================
+# class Clack
+
+#   #---------------------------------------------------------------------------------------------------------
+#   intro_outro: ->
+#     CLK.intro "create-my-app"
+#     value = await CLK.text { message: "press CTRL+C to continue", }
+#     if CLK.isCancel value
+#       CLK.cancel "Operation cancelled."
+#       # process.exit 0
+#     CLK.outro "You're all set!"
+#     return null
+
+#   #---------------------------------------------------------------------------------------------------------
+#   text: ->
+#     cfg =
+#       message:      "What is your name?"
+#       placeholder:  "Not sure"
+#       initialValue: "Jim"
+#       validate:     ( value ) ->
+#         # debug 'Ω___1', rpr value
+#         return "Value is required!" if value.length is 0
+#         return null
+#     name = await CLK.text cfg
+#     info "Ω___2 your name is #{rpr name}"
+#     return null
+
+#   #---------------------------------------------------------------------------------------------------------
+#   select: ->
+#     await do =>
+#       cfg =
+#         message:    "Pick a project type."
+#         options: [
+#           { value: 'ts',      label: 'TypeScript' },
+#           { value: 'js',      label: 'JavaScript' },
+#           { value: 'coffee',  label: 'CoffeeScript', hint: 'yes!' }, ]
+#       project_type = await CLK.select cfg
+#       info "Ω___3 project type: #{rpr project_type}"
+#       return null
+#     await do =>
+#       spinner = CLK.spinner()
+#       spinner.start "asking questions"
+#       cfg =
+#         message:    "Select additional tools."
+#         options: [
+#           { value: 'eslint', label: 'ESLint', hint: 'recommended' },
+#           { value: 'prettier', label: 'Prettier' },
+#           { value: 'gh-action', label: 'GitHub Action' }, ]
+#         required: false
+#       tools = await CLK.multiselect cfg
+#       info "Ω___4 tools: #{rpr tools}"
+#       spinner.stop "thanks!"
+#       return null
+#     return null
+# # clack = new Clack()
+
+###
+
+Errors entail failures
+
+
+###
+
 #===========================================================================================================
-class Clack
+### TAINT Later to be extended so we pass in parameters, not messages ###
+class Dialog_error              extends Error
+class Interactive_dialog_error  extends Dialog_error
+class Programmatic_dialog_error extends Dialog_error
+class Internal_error            extends Programmatic_dialog_error
+class Overrun_error             extends Programmatic_dialog_error
 
-  #---------------------------------------------------------------------------------------------------------
-  intro_outro: ->
-    CLK.intro "create-my-app"
-    value = await CLK.text { message: "press CTRL+C to continue", }
-    if CLK.isCancel value
-      CLK.cancel "Operation cancelled."
-      # process.exit 0
-    CLK.outro "You're all set!"
-    return null
 
-  #---------------------------------------------------------------------------------------------------------
-  text: ->
-    cfg =
-      message:      "What is your name?"
-      placeholder:  "Not sure"
-      initialValue: "Jim"
-      validate:     ( value ) ->
-        # debug 'Ω___1', rpr value
-        return "Value is required!" if value.length is 0
-        return null
-    name = await CLK.text cfg
-    info "Ω___2 your name is #{rpr name}"
-    return null
+#===========================================================================================================
+### TAINT Later to be extended so we pass in parameters, not messages ###
+class Dialog_failure
+  constructor: ( @message ) -> undefined
 
-  #---------------------------------------------------------------------------------------------------------
-  select: ->
-    await do =>
-      cfg =
-        message:    "Pick a project type."
-        options: [
-          { value: 'ts',      label: 'TypeScript' },
-          { value: 'js',      label: 'JavaScript' },
-          { value: 'coffee',  label: 'CoffeeScript', hint: 'yes!' }, ]
-      project_type = await CLK.select cfg
-      info "Ω___3 project type: #{rpr project_type}"
-      return null
-    await do =>
-      spinner = CLK.spinner()
-      spinner.start "asking questions"
-      cfg =
-        message:    "Select additional tools."
-        options: [
-          { value: 'eslint', label: 'ESLint', hint: 'recommended' },
-          { value: 'prettier', label: 'Prettier' },
-          { value: 'gh-action', label: 'GitHub Action' }, ]
-        required: false
-      tools = await CLK.multiselect cfg
-      info "Ω___4 tools: #{rpr tools}"
-      spinner.stop "thanks!"
-      return null
-    return null
-# clack = new Clack()
+class Misstep_failure   extends Dialog_failure
+class Underrun_failure  extends Dialog_failure
+class Overrun_failure   extends Dialog_failure
+class Internal_failure  extends Dialog_failure
+
 
 #===========================================================================================================
 class Interactive_dialog
@@ -97,47 +124,67 @@ class Interactive_dialog
   intro:    ( cfg ) -> @ctrlc await CLK.intro cfg
   outro:    ( cfg ) -> @ctrlc await CLK.outro cfg
   confirm:  ( cfg ) -> @ctrlc await CLK.confirm cfg
+  finish:           -> null
 
   #---------------------------------------------------------------------------------------------------------
   process_exit: ( code = 0 ) -> process.exit code
 
 
 #===========================================================================================================
-class Programmatic_dialog_error extends Error
-
-#===========================================================================================================
 class Programmatic_dialog
 
   #---------------------------------------------------------------------------------------------------------
   constructor: ( steps ) ->
-    @exp_steps  = steps
-    @pc         = -1
-    @act_steps  = []
-    @errors     = []
+    @_exp_steps = steps
+    @_pc        = -1
+    @_act_steps = []
+    #.......................................................................................................
+    GUY.props.def @, '_failures',
+      enumerable:   false
+      configurable: false
+      get:          -> ( d for d in @_act_steps when d instanceof Dialog_failure )
+    #.......................................................................................................
     return undefined
 
   #---------------------------------------------------------------------------------------------------------
   _next: ->
-    @pc++
-    unless ( R = @exp_steps[ @pc ] ? null )?
-      throw new Programmatic_dialog_error "running out of steps: #{rpr @}"
+    @_pc++
+    unless ( R = @_exp_steps[ @_pc ] ? null )?
+      message = "emergency halt, running too long: act #{@_act_steps.length + 1} exp #{@_exp_steps.length}"
+      @_fail new Overrun_failure message
+      throw new Overrun_error message
     return R
 
   #---------------------------------------------------------------------------------------------------------
-  _error: ( message ) ->
-    error = { isa: 'error', message, }
-    @act_steps.push error
-    @errors.push    error
+  _fail: ( failure ) ->
+    @_act_steps.push  failure
+    @_failures.push   failure
     return null
 
   #---------------------------------------------------------------------------------------------------------
   _step: ( act_key ) ->
     [ exp_key, value, ] = @_next()
     if act_key is exp_key
-      @act_steps.push act_key
+      @_act_steps.push act_key
     else
-      @_error "error @ #{@pc}: act #{rpr act_key}, exp #{rpr exp_key}"
+      @_act_steps.push new Misstep_failure "step##{@_pc}: act #{rpr act_key}, exp #{rpr exp_key}"
     return value
+
+  #---------------------------------------------------------------------------------------------------------
+  _is_finished: -> @_act_steps.length is @_exp_steps.length
+  _is_underrun: -> @_act_steps.length < @_exp_steps.length
+  _is_overrun:  -> @_act_steps.length > @_exp_steps.length
+
+  #---------------------------------------------------------------------------------------------------------
+  finish: ( P... ) ->
+    #### `dlg.finish()` should be called after the simulated dialog has ben run to issue an  ####
+    return true if @_is_finished() or @_is_overrun()
+    # unless @_is_underrun()
+    #   message = "should have recognized overrun"
+    #   @_fail new Internal_failure message
+    #   throw new Internal_error message
+    @_fail new Underrun_failure "finished too early: act #{@_act_steps.length} exp #{@_exp_steps.length}"
+    return false
 
   #---------------------------------------------------------------------------------------------------------
   intro:    ( P... ) -> @_step 'intro'
@@ -172,18 +219,21 @@ demo_run_interactive = ->
 #-----------------------------------------------------------------------------------------------------------
 demo_run_programmatic = ->
   steps = [
-    [ 'intro',    true, ]
-    [ 'confirm',  true, ]
-    # [ 'confirm',  false, ]
-    [ 'outro',    true, ] ]
+    [ 'intro', ]
+    [ 'confirm',  false, ]
+    # [ 'confirm',  true, ]
+    [ 'outro', ]
+    [ 'outro', ]
+    ]
   dlg = new Programmatic_dialog steps
   try
     await sample_dialog dlg
   catch error
     throw error unless error instanceof Programmatic_dialog_error
-    warn 'Ω__14', reverse bold error.message
-  warn 'Ω__15', dlg.errors if dlg.errors.length > 0
-  help 'Ω__16', dlg.act_steps
+    warn 'Ω___7', reverse bold error.message
+  dlg.finish()
+  warn 'Ω___9', dlg._failures
+  help 'Ω__10', dlg._act_steps
   return null
 
 
