0b6e944cf49eb3a2025226b3b7b018e755c3e791
loveencounterflow <flow@home>
2024-11-01 10:59:04 +0100

    update

--- dev/diatribe/lib/test-all.js
+++ dev/diatribe/lib/test-all.js
@@ -1,6 +1,6 @@
 (async function() {
   'use strict';
-  var GTNG, GUY, Test, WGUY, alert, debug, echo, help, info, inspect, log, plain, praise, reverse, rpr, types, urge, warn, whisper;
+  var GTNG, GUY, Test, WGUY, alert, debug, demo_run_dlg1_interactive, demo_run_dlg1_programmatic, echo, help, info, inspect, log, plain, praise, reverse, rpr, run_dlg1, types, urge, warn, whisper;
 
   GUY = require('guy');
 
@@ -15,6 +15,126 @@
   GTNG = require('../../../apps/guy-test-NG');
 
   ({Test} = GTNG);
+
+  //===========================================================================================================
+  run_dlg1 = async function(dlg) {
+    var value;
+    dlg.intro("create-my-app");
+    while (true) {
+      //.........................................................................................................
+      if (value = (await dlg.confirm({
+        ref: 'q1',
+        message: "do you want to loop?"
+      }))) {
+        debug('Ω___5', rpr(value));
+        continue;
+      }
+      break;
+    }
+    await dlg.text({
+      ref: 'q2',
+      message: "please enter text"
+    });
+    await (async() => {      //.........................................................................................................
+      var cfg, project_type;
+      cfg = {
+        ref: 'q3',
+        message: "Pick a project type.",
+        options: [
+          {
+            value: 'ts',
+            label: 'TypeScript'
+          },
+          {
+            value: 'js',
+            label: 'JavaScript'
+          },
+          {
+            value: 'coffee',
+            label: 'CoffeeScript',
+            hint: 'yes!'
+          }
+        ]
+      };
+      project_type = (await dlg.select(cfg));
+      return null;
+    })();
+    await (async() => {      //.........................................................................................................
+      var cfg, spinner, tools;
+      spinner = dlg.get_spinner();
+      spinner.start("asking questions");
+      cfg = {
+        ref: null, // intentionally left out
+        message: "Select additional tools.",
+        options: [
+          {
+            value: 'eslint',
+            label: 'ESLint',
+            hint: 'recommended'
+          },
+          {
+            value: 'prettier',
+            label: 'Prettier'
+          },
+          {
+            value: 'gh-action',
+            label: 'GitHub Action'
+          }
+        ],
+        required: false
+      };
+      tools = (await dlg.multiselect(cfg));
+      spinner.stop("thanks!");
+      return null;
+    })();
+    //.........................................................................................................
+    dlg.outro("You're all set!");
+    //.........................................................................................................
+    return dlg.results;
+  };
+
+  //===========================================================================================================
+  demo_run_dlg1_interactive = async function() {
+    var Interactive_dialog;
+    ({Interactive_dialog} = require('../../../apps/diatribe'));
+    await run_dlg1(new Interactive_dialog());
+    return null;
+  };
+
+  //-----------------------------------------------------------------------------------------------------------
+  demo_run_dlg1_programmatic = async function() {
+    var Programmatic_dialog, dlg, error, errors, ref, ref1, step, steps;
+    ({Programmatic_dialog, errors} = require('../../../apps/diatribe'));
+    steps = {
+      q1: ['confirm', false],
+      q2: ['text', "helo"],
+      q3: ['select', 'coffee'],
+      $q4: ['multiselect', ['prettier']],
+      whatever: ['select', 'js']
+    };
+    dlg = new Programmatic_dialog(steps);
+    try {
+      await run_dlg1(dlg);
+    } catch (error1) {
+      error = error1;
+      if (!(error instanceof errors.Dialog_error)) {
+        throw error;
+      }
+      warn('Ω___8', reverse(bold(error.message)));
+    }
+    dlg.finish();
+    ref1 = dlg._act_steps;
+    for (ref in ref1) {
+      step = ref1[ref];
+      if (step instanceof errors.Dialog_failure) {
+        warn(ref, step);
+      } else {
+        help(ref, step);
+      }
+    }
+    info(dlg.results);
+    return null;
+  };
 
   //###########################################################################################################
 
@@ -34,10 +154,12 @@
 
   //===========================================================================================================
   if (module === require.main) {
-    await (() => {
-      return (new Test({
+    await (async() => {
+      (new Test({
         throw_on_error: false
       })).test(this.diatribe_tasks);
+      // await demo_run_dlg1_interactive()
+      return (await demo_run_dlg1_programmatic());
     })();
   }
 
--- dev/diatribe/src/test-all.coffee
+++ dev/diatribe/src/test-all.coffee
@@ -22,6 +22,75 @@ types                     = new ( require 'intertype' ).Intertype()
 GTNG                      = require '../../../apps/guy-test-NG'
 { Test                  } = GTNG
 
+#===========================================================================================================
+run_dlg1 = ( dlg ) ->
+  dlg.intro "create-my-app"
+  #.........................................................................................................
+  loop
+    if value = await dlg.confirm { ref: 'q1', message: "do you want to loop?", }
+      debug 'Ω___5', rpr value
+      continue
+    break
+  await dlg.text { ref: 'q2', message: "please enter text", }
+  #.........................................................................................................
+  await do =>
+    cfg =
+      ref:        'q3'
+      message:    "Pick a project type."
+      options: [
+        { value: 'ts',      label: 'TypeScript' },
+        { value: 'js',      label: 'JavaScript' },
+        { value: 'coffee',  label: 'CoffeeScript', hint: 'yes!' }, ]
+    project_type = await dlg.select cfg
+    return null
+  #.........................................................................................................
+  await do =>
+    spinner = dlg.get_spinner()
+    spinner.start "asking questions"
+    cfg =
+      ref:        null # intentionally left out
+      message:    "Select additional tools."
+      options: [
+        { value: 'eslint', label: 'ESLint', hint: 'recommended' },
+        { value: 'prettier', label: 'Prettier' },
+        { value: 'gh-action', label: 'GitHub Action' }, ]
+      required: false
+    tools = await dlg.multiselect cfg
+    spinner.stop "thanks!"
+    return null
+  #.........................................................................................................
+  dlg.outro "You're all set!"
+  #.........................................................................................................
+  return dlg.results
+
+#===========================================================================================================
+demo_run_dlg1_interactive = ->
+  { Interactive_dialog, } = require '../../../apps/diatribe'
+  await run_dlg1 new Interactive_dialog()
+  return null
+
+#-----------------------------------------------------------------------------------------------------------
+demo_run_dlg1_programmatic = ->
+  { Programmatic_dialog, errors, } = require '../../../apps/diatribe'
+  steps =
+    q1:         [ 'confirm',      false,            ]
+    q2:         [ 'text',         "helo",           ]
+    q3:         [ 'select',       'coffee',         ]
+    $q4:        [ 'multiselect',  [ 'prettier', ],  ]
+    whatever:   [ 'select',       'js',         ]
+  dlg = new Programmatic_dialog steps
+  try
+    await run_dlg1 dlg
+  catch error
+    throw error unless error instanceof errors.Dialog_error
+    warn 'Ω___8', reverse bold error.message
+  dlg.finish()
+  for ref, step of dlg._act_steps
+    if step instanceof errors.Dialog_failure then warn ref, step
+    else                                          help ref, step
+  info dlg.results
+  return null
+
 
 
 ############################################################################################################
@@ -39,5 +108,5 @@ GTNG                      = require '../../../apps/guy-test-NG'
 #===========================================================================================================
 if module is require.main then await do =>
   ( new Test { throw_on_error: false, } ).test @diatribe_tasks
-
-
+  # await demo_run_dlg1_interactive()
+  await demo_run_dlg1_programmatic()
