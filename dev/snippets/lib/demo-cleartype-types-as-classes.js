e1b6e66811effd07375cc48f7d9d0d17809dcfdb
loveencounterflow <flow@home>
2025-06-15 21:14:46 +0200

    update

--- dev/snippets/lib/demo-cleartype-types-as-classes.js
+++ dev/snippets/lib/demo-cleartype-types-as-classes.js
@@ -1,4 +1,37 @@
 (async function() {
+  /*
+
+  1.  ```
+      class Type
+        ...
+      type      = new Type type_cfg
+      integer   = type.$create { $name: 'integer',  $isa: ( -> ), ..., }
+      quantity  = type.$create { $name: 'quantity', $isa: ( -> ), ..., q: ..., u: ..., }
+
+   * usage:
+      integer.$create 3.141
+      ```
+
+  2.  ```
+      class Integer extends Type
+        $isa:     ( x ) -> Number.isInteger x
+        $create:  ( x ) -> parseInt x, 10
+      integer = new Integer()
+
+   * usage:
+      integer.$create 3.141
+      ```
+
+  3.  ```
+      class integer extends Type
+        @$isa:    ( x ) -> Number.isInteger x
+        @$create: ( x ) -> parseInt x, 10
+
+   * usage:
+      integer.$create 3.141
+      ```
+
+   */
   'use strict';
   var GUY, alert, bold, debug, echo, help, hide, info, inspect, log, nameit, plain, praise, require_cleartype, reverse, rpr, urge, warn, whisper;
 
@@ -17,103 +50,208 @@
 
   //===========================================================================================================
   require_cleartype = function() {
-    var integer, quantity, std;
-    //=========================================================================================================
-    integer = class integer {
-      constructor(n = 0) {
-        parseInt(n, 10);
+    var Type, float, integer, nonempty_text, quantity, std, text, type;
+    //===========================================================================================================
+    Type = class Type {
+      //---------------------------------------------------------------------------------------------------------
+      constructor() {
+        return void 0;
       }
 
+      //---------------------------------------------------------------------------------------------------------
+      $create(dcl) {
+        /* TAINT check whether there are fields */
+        var $create, $isa, R, dsc, field_name, fields, ref, ref1;
+        if (dcl.$isa != null) {
+          $isa = function(x) {
+            return dcl.$isa(x);
+          };
+        } else {
+          fields = {};
+          ref = Object.getOwnPropertyDescriptors(dcl);
+          for (field_name in ref) {
+            dsc = ref[field_name];
+            if (field_name.startsWith('$')) {
+              continue;
+            }
+            fields[field_name] = dsc.value;
+          }
+          // field_names = Object.keys fields
+          $isa = function(x) {
+            var ref1, subtype;
+            if (x == null) {
+              return false;
+            }
+            if ((ref1 = x.constructor) !== Object && ref1 !== (void 0)) {
+              return false;
+            }
+/* stad.pod.$isa x */            for (field_name in fields) {
+              subtype = fields[field_name];
+              if (subtype.$isa(x[field_name])) {
+                continue;
+              }
+              warn('Ω___1', `x.${field_name} is not a ${subtype.$name}`);
+              return false;
+            }
+            return true;
+          };
+        }
+        //.......................................................................................................
+        $create = (ref1 = dcl.$create) != null ? ref1 : function() {};
+        // if dcl.$create?
+        //   $create = ( x ) -> dcl.$create x
+        // else
+        //   ### TAINT check whether there are fields ###
+        //   fields = {}
+        //   for field_name, dsc of Object.getOwnPropertyDescriptors dcl
+
+        //.......................................................................................................
+        R = {};
+        R.$isa = $isa.bind(R);
+        R.$create = $create.bind(R);
+        return R;
+      }
+
+      //---------------------------------------------------------------------------------------------------------
+      $validate(x) {}
+
+      $isa(x) {}
+
     };
-    //=========================================================================================================
-    quantity = class quantity {
-      constructor(cfg) {
+    //-----------------------------------------------------------------------------------------------------------
+    type = new Type();
+    //-----------------------------------------------------------------------------------------------------------
+    text = type.$create({
+      $name: 'text',
+      $isa: function(x) {
+        return (Object.prototype.toString.call(x)) === '[object String]';
+      },
+      $create: function(x) {
+        var ref;
+        return (ref = x != null ? x.toString() : void 0) != null ? ref : '';
+      }
+    });
+    //-----------------------------------------------------------------------------------------------------------
+    nonempty_text = type.$create({
+      $name: 'nonempty_text',
+      $isa: function(x) {
+        return (text.$isa(x)) && (x.length !== 0);
+      },
+      $create: function(x) {
+        var ref;
+        return (ref = x != null ? x.toString() : void 0) != null ? ref : '';
+      }
+    });
+    //-----------------------------------------------------------------------------------------------------------
+    float = type.$create({
+      $name: 'float',
+      $isa: function(x) {
+        return Number.isFinite(x);
+      },
+      $create: function(n = 0) {
+        if (typeof x !== "undefined" && x !== null) {
+          return parseFloat(x);
+        } else {
+          return 0;
+        }
+      }
+    });
+    //-----------------------------------------------------------------------------------------------------------
+    quantity = (() => {
+      var $create, q, u;
+      $create = function(cfg) {
         return {
           q: 0,
           u: 'u',
           ...cfg
         };
-      }
-
-      my_field_names() {
-        var k, results;
-        results = [];
-        for (k in this) {
-          results.push(k);
+      };
+      q = type.$create({
+        $name: 'q',
+        $isa: function(x) {
+          return float.$isa(x);
         }
-        return results;
-      }
-
-    };
-    /*
-
-    1.  ```
-        integer = type.$create { $name: 'integer', $isa: ( -> ), ..., }
-
-     * usage:
-        integer.$create 3.141
-        ```
-
-    2.  ```
-        class Integer extends Type
-          $isa:     ( x ) -> Number.isInteger x
-          $create:  ( x ) -> parseInt x, 10
-        integer = new Integer()
-
-     * usage:
-        integer.$create 3.141
-        ```
-
-    3.  ```
-        class integer extends Type
-          @$isa:    ( x ) -> Number.isInteger x
-          @$create: ( x ) -> parseInt x, 10
-
-     * usage:
-        integer.$create 3.141
-        ```
-
-     */
+      });
+      u = type.$create({
+        $name: 'u',
+        $isa: function(x) {
+          return nonempty_text.$isa(x);
+        }
+      });
+      return type.$create({
+        $name: 'quantity',
+        $create,
+        q,
+        u
+      });
+    })();
+    //-----------------------------------------------------------------------------------------------------------
+    integer = (() => {
+      var $create, $isa;
+      $isa = function(x) {
+        return Number.isInteger(x);
+      };
+      $create = function(n = 0) {
+        if (typeof x !== "undefined" && x !== null) {
+          return parseInt(n, 10);
+        } else {
+          return 0;
+        }
+      };
+      return type.$create({
+        $name: 'integer',
+        $isa,
+        $create
+      });
+    })();
+    //===========================================================================================================
+    std = {text, nonempty_text, float, integer, quantity};
     //=========================================================================================================
-    std = {};
-    //=========================================================================================================
-    return {std, integer, quantity};
+    return {std, Type};
   };
 
   //===========================================================================================================
   if (module === require.main) {
     await (() => {
-      var integer, quantity, std;
-      ({integer, quantity, std} = require_cleartype());
-      info('Ω___1', std);
+      var Type, std;
+      ({Type, std} = require_cleartype());
+      info('Ω___2', std);
       (() => {
-        var d;
         echo();
-        info('Ω___2', integer);
-        info('Ω___3', d = new integer(3.141));
-        info('Ω___4', d.constructor === Object);
-        info('Ω___5', d.constructor === integer);
-        info('Ω___6', 7 + d);
-        return info('Ω___7', d + 7);
+        info('Ω___3', std.integer);
+        info('Ω___4', std.integer.$isa(3.141));
+        info('Ω___5', std.integer.$isa(3));
+        info('Ω___6', std.integer.$create('3'));
+        return info('Ω___7', std.integer.$create());
+      })();
+      (() => {
+        echo();
+        info('Ω___8', std.nonempty_text);
+        info('Ω___9', std.nonempty_text.$isa(3.141));
+        info('Ω__10', std.nonempty_text.$isa(''));
+        info('Ω__11', std.nonempty_text.$isa('d'));
+        info('Ω__12', std.nonempty_text.$create());
+        info('Ω__13', std.nonempty_text.$create(false));
+        return info('Ω__14', std.nonempty_text.$create('d'));
       })();
       return (() => {
-        var d;
         echo();
-        info('Ω___8', quantity);
-        info('Ω___9', d = new quantity({
-          q: 7,
+        info('Ω__15', std.quantity);
+        info('Ω__16', std.quantity.$create());
+        info('Ω__17', std.quantity.$create({
+          q: 4.3,
           u: 's'
         }));
-        info('Ω__10', d.constructor === Object);
-        info('Ω__11', d.constructor === quantity);
-        info('Ω__12', d.q, d.u);
-        info('Ω__13', {...d});
-        return info('Ω__14', d.my_field_names);
+        info('Ω__18', std.quantity.$isa({
+          q: 4.3,
+          u: 's'
+        }));
+        info('Ω__19', std.quantity.q.$isa(7));
+        info('Ω__20', std.quantity.q.$isa(2e308));
+        return info('Ω__21', std.quantity.u.$create('g'));
       })();
     })();
   }
-
-  // info 'Ω__15', d.my_field_names()
 
 }).call(this);
 
--- dev/snippets/src/demo-cleartype-types-as-classes.coffee
+++ dev/snippets/src/demo-cleartype-types-as-classes.coffee
@@ -1,4 +1,39 @@
 
+###
+
+1.  ```
+    class Type
+      ...
+    type      = new Type type_cfg
+    integer   = type.$create { $name: 'integer',  $isa: ( -> ), ..., }
+    quantity  = type.$create { $name: 'quantity', $isa: ( -> ), ..., q: ..., u: ..., }
+
+    # usage:
+    integer.$create 3.141
+    ```
+
+2.  ```
+    class Integer extends Type
+      $isa:     ( x ) -> Number.isInteger x
+      $create:  ( x ) -> parseInt x, 10
+    integer = new Integer()
+
+    # usage:
+    integer.$create 3.141
+    ```
+
+3.  ```
+    class integer extends Type
+      @$isa:    ( x ) -> Number.isInteger x
+      @$create: ( x ) -> parseInt x, 10
+
+    # usage:
+    integer.$create 3.141
+    ```
+
+###
+
+
 'use strict'
 
 #===========================================================================================================
@@ -26,74 +61,120 @@ GUY                       = require 'guy'
 #===========================================================================================================
 require_cleartype = ->
 
-  #=========================================================================================================
-  class integer
-    constructor: ( n = 0 ) -> parseInt n, 10
+  #===========================================================================================================
+  class Type
+
+    #---------------------------------------------------------------------------------------------------------
+    constructor: ->
+      return undefined
+
+    #---------------------------------------------------------------------------------------------------------
+    $create: ( dcl ) ->
+      if dcl.$isa?
+        $isa = ( x ) -> dcl.$isa x
+      else
+        ### TAINT check whether there are fields ###
+        fields = {}
+        for field_name, dsc of Object.getOwnPropertyDescriptors dcl
+          continue if field_name.startsWith '$'
+          fields[ field_name ] = dsc.value
+        # field_names = Object.keys fields
+        $isa = ( x ) ->
+          return false unless x?
+          return false unless x.constructor in [ Object, undefined, ] ### stad.pod.$isa x ###
+          for field_name, subtype of fields
+            continue if subtype.$isa x[ field_name ]
+            warn 'Ω___1', "x.#{field_name} is not a #{subtype.$name}"
+            return false
+          return true
+      #.......................................................................................................
+      $create = dcl.$create ? ->
+      # if dcl.$create?
+      #   $create = ( x ) -> dcl.$create x
+      # else
+      #   ### TAINT check whether there are fields ###
+      #   fields = {}
+      #   for field_name, dsc of Object.getOwnPropertyDescriptors dcl
+
+      #.......................................................................................................
+      R = {}
+      R.$isa    = $isa.bind R
+      R.$create = $create.bind R
+      return R
+
+    #---------------------------------------------------------------------------------------------------------
+    $validate: ( x ) ->
+    $isa: ( x ) ->
+
+  #-----------------------------------------------------------------------------------------------------------
+  type = new Type()
+
+  #-----------------------------------------------------------------------------------------------------------
+  text = type.$create
+    $name:     'text'
+    $isa:     ( x ) -> ( Object::toString.call x ) is '[object String]'
+    $create:  ( x ) -> x?.toString() ? ''
+  #-----------------------------------------------------------------------------------------------------------
+  nonempty_text = type.$create
+    $name:     'nonempty_text'
+    $isa:     ( x ) -> ( text.$isa x ) and ( x.length isnt 0 )
+    $create:  ( x ) -> x?.toString() ? ''
+  #-----------------------------------------------------------------------------------------------------------
+  float = type.$create
+    $name:     'float'
+    $isa:     ( x ) -> Number.isFinite x
+    $create:  ( n = 0 ) -> if x? then ( parseFloat x ) else 0
+  #-----------------------------------------------------------------------------------------------------------
+  quantity = do =>
+    $create = ( cfg ) -> { q: 0, u: 'u', cfg..., }
+    q       = type.$create
+      $name: 'q'
+      $isa: ( x ) -> float.$isa x
+    u       = type.$create
+      $name: 'u'
+      $isa: ( x ) -> nonempty_text.$isa x
+    type.$create { $name: 'quantity', $create, q, u, }
+  #-----------------------------------------------------------------------------------------------------------
+  integer = do =>
+    $isa    = ( x ) -> Number.isInteger x
+    $create = ( n = 0 ) -> if x? then ( parseInt n, 10 ) else 0
+    type.$create { $name: 'integer', $isa, $create, }
+
+  #===========================================================================================================
+  std = { text, nonempty_text, float, integer, quantity, }
 
   #=========================================================================================================
-  class quantity
-    constructor: ( cfg ) ->
-      return { q: 0, u: 'u', cfg..., }
-    my_field_names: -> ( k for k of @ )
-
-  ###
-
-  1.  ```
-      integer = type.$create { $name: 'integer', $isa: ( -> ), ..., }
-
-      # usage:
-      integer.$create 3.141
-      ```
-
-  2.  ```
-      class Integer extends Type
-        $isa:     ( x ) -> Number.isInteger x
-        $create:  ( x ) -> parseInt x, 10
-      integer = new Integer()
-
-      # usage:
-      integer.$create 3.141
-      ```
-
-  3.  ```
-      class integer extends Type
-        @$isa:    ( x ) -> Number.isInteger x
-        @$create: ( x ) -> parseInt x, 10
-
-      # usage:
-      integer.$create 3.141
-      ```
-
-  ###
-  #=========================================================================================================
-  std = {}
-
-  #=========================================================================================================
-  return { std, integer, quantity, }
+  return { std, Type, }
 
 
 #===========================================================================================================
 if module is require.main then await do =>
-  { integer
-    quantity
+  { Type
     std             } = require_cleartype()
-  info 'Ω___1', std
+  info 'Ω___2', std
   do =>
     echo()
-    info 'Ω___2', integer
-    info 'Ω___3', d = new integer 3.141
-    info 'Ω___4', d.constructor is Object
-    info 'Ω___5', d.constructor is integer
-    info 'Ω___6', 7 + d
-    info 'Ω___7', d + 7
+    info 'Ω___3', std.integer
+    info 'Ω___4', std.integer.$isa 3.141
+    info 'Ω___5', std.integer.$isa 3
+    info 'Ω___6', std.integer.$create '3'
+    info 'Ω___7', std.integer.$create()
   do =>
     echo()
-    info 'Ω___8', quantity
-    info 'Ω___9', d = new quantity { q: 7, u: 's', }
-    info 'Ω__10', d.constructor is Object
-    info 'Ω__11', d.constructor is quantity
-    info 'Ω__12', d.q, d.u
-    info 'Ω__13', { d..., }
-    info 'Ω__14', d.my_field_names
-    # info 'Ω__15', d.my_field_names()
+    info 'Ω___8', std.nonempty_text
+    info 'Ω___9', std.nonempty_text.$isa 3.141
+    info 'Ω__10', std.nonempty_text.$isa ''
+    info 'Ω__11', std.nonempty_text.$isa 'd'
+    info 'Ω__12', std.nonempty_text.$create()
+    info 'Ω__13', std.nonempty_text.$create false
+    info 'Ω__14', std.nonempty_text.$create 'd'
+  do =>
+    echo()
+    info 'Ω__15', std.quantity
+    info 'Ω__16', std.quantity.$create()
+    info 'Ω__17', std.quantity.$create  { q: 4.3, u: 's', }
+    info 'Ω__18', std.quantity.$isa     { q: 4.3, u: 's', }
+    info 'Ω__19', std.quantity.q.$isa 7
+    info 'Ω__20', std.quantity.q.$isa Infinity
+    info 'Ω__21', std.quantity.u.$create 'g'
 
