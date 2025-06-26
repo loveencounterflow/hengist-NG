e555bd954c8aefa57f89f7842a70081dbd955a8e
loveencounterflow <flow@home>
2025-06-26 17:48:53 +0200

    update

--- dev/normalize-function-arguments/lib/test-basics.js
+++ dev/normalize-function-arguments/lib/test-basics.js
@@ -27,15 +27,20 @@
       ({nfa, get_signature} = NFA);
       ({red, reverse} = GUY.trm);
       (() => {        //.......................................................................................................
-        var fn, frz, Ωnfat__10, Ωnfat__11, Ωnfat__12, Ωnfat__13, Ωnfat__14, Ωnfat__15, Ωnfat__16, Ωnfat__17, Ωnfat___2, Ωnfat___3, Ωnfat___4, Ωnfat___5, Ωnfat___6, Ωnfat___7, Ωnfat___8, Ωnfat___9;
+        var fn, frz, Ωnfat__10, Ωnfat__11, Ωnfat__12, Ωnfat__13, Ωnfat__14, Ωnfat__15, Ωnfat__16, Ωnfat__17, Ωnfat__18, Ωnfat__19, Ωnfat__20, Ωnfat___1, Ωnfat___2, Ωnfat___3, Ωnfat___4, Ωnfat___5, Ωnfat___6, Ωnfat___7, Ωnfat___8, Ωnfat___9;
         fn = nfa(function(a, b, c, cfg) {
           return {a, b, c, cfg};
         });
-        // fn = ( a, b, c, cfg ) -> { $A: [ arguments..., ], a, b, c, cfg, }
-        // help 'Ω___1', get_fn_args fn
-        // if signature?
         frz = Object.freeze;
         //.....................................................................................................
+        this.eq((Ωnfat___1 = function() {
+          return fn();
+        }), {
+          a: void 0,
+          b: void 0,
+          c: void 0,
+          cfg: {}
+        });
         this.eq((Ωnfat___2 = function() {
           return fn(1);
         }), {
@@ -73,6 +78,14 @@
           return fn(1, 2, 3, 4);
         }), /expected up to 3 positional arguments, got 4/);
         this.eq((Ωnfat___6 = function() {
+          return fn({});
+        }), {
+          a: void 0,
+          b: void 0,
+          c: void 0,
+          cfg: {}
+        });
+        this.eq((Ωnfat___7 = function() {
           return fn(1, {});
         }), {
           a: 1,
@@ -82,7 +95,7 @@
             a: 1
           }
         });
-        this.eq((Ωnfat___7 = function() {
+        this.eq((Ωnfat___8 = function() {
           return fn(1, 2, {});
         }), {
           a: 1,
@@ -93,7 +106,7 @@
             b: 2
           }
         });
-        this.eq((Ωnfat___8 = function() {
+        this.eq((Ωnfat___9 = function() {
           return fn(1, 2, 3, {});
         }), {
           a: 1,
@@ -105,10 +118,22 @@
             c: 3
           }
         });
-        this.throws((Ωnfat___9 = function() {
+        this.throws((Ωnfat__10 = function() {
           return fn(1, 2, 3, 4, {});
         }), /expected up to 3 positional arguments, got 4/);
-        this.eq((Ωnfat__10 = function() {
+        this.eq((Ωnfat__11 = function() {
+          return fn({
+            b: 88
+          });
+        }), {
+          a: void 0,
+          b: 88,
+          c: void 0,
+          cfg: {
+            b: 88
+          }
+        });
+        this.eq((Ωnfat__12 = function() {
           return fn(1, {
             b: 88
           });
@@ -121,7 +146,7 @@
             a: 1
           }
         });
-        this.eq((Ωnfat__11 = function() {
+        this.eq((Ωnfat__13 = function() {
           return fn(1, 2, {
             b: 88
           });
@@ -134,7 +159,7 @@
             a: 1
           }
         });
-        this.eq((Ωnfat__12 = function() {
+        this.eq((Ωnfat__14 = function() {
           return fn(1, 2, 3, {
             b: 88
           });
@@ -148,12 +173,24 @@
             c: 3
           }
         });
-        this.throws((Ωnfat__13 = function() {
+        this.throws((Ωnfat__15 = function() {
           return fn(1, 2, 3, 4, {
             b: 88
           });
         }), /expected up to 3 positional arguments, got 4/);
-        this.eq((Ωnfat__14 = function() {
+        this.eq((Ωnfat__16 = function() {
+          return fn(frz({
+            b: 88
+          }));
+        }), {
+          a: void 0,
+          b: 88,
+          c: void 0,
+          cfg: {
+            b: 88
+          }
+        });
+        this.eq((Ωnfat__17 = function() {
           return fn(1, frz({
             b: 88
           }));
@@ -166,7 +203,7 @@
             a: 1
           }
         });
-        this.eq((Ωnfat__15 = function() {
+        this.eq((Ωnfat__18 = function() {
           return fn(1, 2, frz({
             b: 88
           }));
@@ -179,7 +216,7 @@
             a: 1
           }
         });
-        this.eq((Ωnfat__16 = function() {
+        this.eq((Ωnfat__19 = function() {
           return fn(1, 2, 3, frz({
             b: 88
           }));
@@ -193,7 +230,7 @@
             c: 3
           }
         });
-        this.throws((Ωnfat__17 = function() {
+        this.throws((Ωnfat__20 = function() {
           return fn(1, 2, 3, 4, frz({
             b: 88
           }));
@@ -202,28 +239,124 @@
         return null;
       })();
       (() => {        //.......................................................................................................
-        var optional, Ωnfat__18, Ωnfat__19, Ωnfat__20, Ωnfat__21, Ωnfat__22, Ωnfat__23, Ωnfat__24;
+        var fn, frz, Ωnfat__21, Ωnfat__22, Ωnfat__23, Ωnfat__24, Ωnfat__25, Ωnfat__26, Ωnfat__27, Ωnfat__28;
+        fn = nfa(function(cfg) {
+          return {cfg};
+        });
+        frz = Object.freeze;
+        //.....................................................................................................
+        this.eq((Ωnfat__21 = function() {
+          return fn();
+        }), {
+          cfg: {}
+        });
+        this.throws((Ωnfat__22 = function() {
+          return fn(1, 2, 3, 4);
+        }), /expected up to 0 positional arguments, got 4/);
+        this.eq((Ωnfat__23 = function() {
+          return fn({});
+        }), {
+          cfg: {}
+        });
+        this.throws((Ωnfat__24 = function() {
+          return fn(1, 2, 3, 4, {});
+        }), /expected up to 0 positional arguments, got 4/);
+        this.eq((Ωnfat__25 = function() {
+          return fn({
+            b: 88
+          });
+        }), {
+          cfg: {
+            b: 88
+          }
+        });
+        this.throws((Ωnfat__26 = function() {
+          return fn(1, 2, 3, 4, {
+            b: 88
+          });
+        }), /expected up to 0 positional arguments, got 4/);
+        this.eq((Ωnfat__27 = function() {
+          return fn(frz({
+            b: 88
+          }));
+        }), {
+          cfg: {
+            b: 88
+          }
+        });
+        this.throws((Ωnfat__28 = function() {
+          return fn(1, 2, 3, 4, frz({
+            b: 88
+          }));
+        }), /expected up to 0 positional arguments, got 4/);
+        //.....................................................................................................
+        return null;
+      })();
+      (() => {        //.......................................................................................................
+        var fn, frz, Ωnfat__29, Ωnfat__30, Ωnfat__31, Ωnfat__32, Ωnfat__33, Ωnfat__34, Ωnfat__35, Ωnfat__36;
+        fn = nfa(function() {
+          return {};
+        });
+        frz = Object.freeze;
+        //.....................................................................................................
+        this.eq((Ωnfat__29 = function() {
+          return fn();
+        }), {});
+        this.throws((Ωnfat__30 = function() {
+          return fn(1, 2, 3, 4);
+        }), /expected up to 0 positional arguments, got 4/);
+        this.throws((Ωnfat__31 = function() {
+          return fn({});
+        }), /expected up to 0 named arguments objects, got 1/);
+        this.throws((Ωnfat__32 = function() {
+          return fn(1, 2, 3, 4, {});
+        }), /expected up to 0 named arguments objects, got 1/);
+        this.throws((Ωnfat__33 = function() {
+          return fn({
+            b: 88
+          });
+        }), /expected up to 0 named arguments objects, got 1/);
+        this.throws((Ωnfat__34 = function() {
+          return fn(1, 2, 3, 4, {
+            b: 88
+          });
+        }), /expected up to 0 named arguments objects, got 1/);
+        this.throws((Ωnfat__35 = function() {
+          return fn(frz({
+            b: 88
+          }));
+        }), /expected up to 0 named arguments objects, got 1/);
+        this.throws((Ωnfat__36 = function() {
+          return fn(1, 2, 3, 4, frz({
+            b: 88
+          }));
+        }), /expected up to 0 named arguments objects, got 1/);
+        //.....................................................................................................
+        return null;
+      })();
+      (() => {        //.......................................................................................................
+        var optional, Ωnfat__37, Ωnfat__38, Ωnfat__39, Ωnfat__40, Ωnfat__41, Ωnfat__43, Ωnfat__44;
         optional = null;
         
       const empty_fn = function (
 
         ) {};
       ;
-        this.eq((Ωnfat__18 = function() {
+        this.eq((Ωnfat__37 = function() {
           return get_signature(function(a) {});
         }), {
           a: 'bare'
         });
-        this.throws((Ωnfat__19 = function() {
+        this.throws((Ωnfat__38 = function() {
           return get_signature(function(a = optional) {});
         }), /not compliant/);
-        this.eq((Ωnfat__20 = function() {
+        this.eq((Ωnfat__39 = function() {
           return get_signature(empty_fn);
         }), {});
-        this.eq((Ωnfat__21 = function() {
+        this.eq((Ωnfat__40 = function() {
           return get_signature(function() {});
         }), {});
-        this.eq((Ωnfat__22 = function() {
+        this.eq((Ωnfat__41 = function() {
           return get_signature(function(a, b, c) {});
         }), {
           a: 'bare',
@@ -231,14 +364,14 @@
           c: 'bare'
         });
         // ### TAINT limitation of CoffeeScript: signature runs up to soak, trailing paramters handled inside function body ###
-        // @eq     ( Ωnfat__23 = -> get_signature ( a, b..., c               ) ->  ), { a: 'bare', b: 'soak', }
-        this.throws((Ωnfat__23 = function() {
+        // @eq     ( Ωnfat__42 = -> get_signature ( a, b..., c               ) ->  ), { a: 'bare', b: 'soak', }
+        this.throws((Ωnfat__43 = function() {
           return get_signature(function(a, ...b) {
             var c, ref;
             ref = b, [...b] = ref, [c] = splice.call(b, -1);
           });
         }), /not compliant/);
-        return this.throws((Ωnfat__24 = function() {
+        return this.throws((Ωnfat__44 = function() {
           return get_signature(function(a, b = null) {});
         }), /not compliant/);
       })();
--- dev/normalize-function-arguments/src/test-basics.coffee
+++ dev/normalize-function-arguments/src/test-basics.coffee
@@ -38,27 +38,58 @@ GTNG                      = require '../../../apps/guy-test-NG'
     #.......................................................................................................
     do =>
       fn = nfa ( a, b, c, cfg ) -> { a, b, c, cfg, }
-      # fn = ( a, b, c, cfg ) -> { $A: [ arguments..., ], a, b, c, cfg, }
-      # help 'Ω___1', get_fn_args fn
-      # if signature?
       frz = Object.freeze
       #.....................................................................................................
-      @eq     ( Ωnfat___2 = -> fn 1                           ), { a: 1, b: undefined, c: undefined, cfg: { a: 1 } }
-      @eq     ( Ωnfat___3 = -> fn 1, 2                        ), { a: 1, b: 2,         c: undefined, cfg: { a: 1, b: 2 } }
-      @eq     ( Ωnfat___4 = -> fn 1, 2, 3                     ), { a: 1, b: 2,         c: 3,         cfg: { a: 1, b: 2, c: 3 } }
+      @eq     ( Ωnfat___1 = -> fn()                           ), { a: undefined, b: undefined, c: undefined, cfg: {} }
+      @eq     ( Ωnfat___2 = -> fn 1                           ), { a: 1,         b: undefined, c: undefined, cfg: { a: 1 } }
+      @eq     ( Ωnfat___3 = -> fn 1, 2                        ), { a: 1,         b: 2,         c: undefined, cfg: { a: 1, b: 2 } }
+      @eq     ( Ωnfat___4 = -> fn 1, 2, 3                     ), { a: 1,         b: 2,         c: 3,         cfg: { a: 1, b: 2, c: 3 } }
       @throws ( Ωnfat___5 = -> fn 1, 2, 3, 4                  ), /expected up to 3 positional arguments, got 4/
-      @eq     ( Ωnfat___6 = -> fn 1,              {}          ), { a: 1, b: undefined, c: undefined, cfg: { a: 1 } }
-      @eq     ( Ωnfat___7 = -> fn 1, 2,           {}          ), { a: 1, b: 2,         c: undefined, cfg: { a: 1, b: 2 } }
-      @eq     ( Ωnfat___8 = -> fn 1, 2, 3,        {}          ), { a: 1, b: 2,         c: 3,         cfg: { a: 1, b: 2, c: 3 } }
-      @throws ( Ωnfat___9 = -> fn 1, 2, 3, 4,     {}          ), /expected up to 3 positional arguments, got 4/
-      @eq     ( Ωnfat__10 = -> fn 1,              { b: 88, }  ), { a: 1, b: 88,        c: undefined, cfg: { b: 88, a: 1 } }
-      @eq     ( Ωnfat__11 = -> fn 1, 2,           { b: 88, }  ), { a: 1, b: 2,         c: undefined, cfg: { b: 2, a: 1 } }
-      @eq     ( Ωnfat__12 = -> fn 1, 2, 3,        { b: 88, }  ), { a: 1, b: 2,         c: 3,         cfg: { b: 2, a: 1, c: 3 } }
-      @throws ( Ωnfat__13 = -> fn 1, 2, 3, 4,     { b: 88, }  ), /expected up to 3 positional arguments, got 4/
-      @eq     ( Ωnfat__14 = -> fn 1,          frz { b: 88, }  ), { a: 1, b: 88,        c: undefined, cfg: { b: 88, a: 1 } }
-      @eq     ( Ωnfat__15 = -> fn 1, 2,       frz { b: 88, }  ), { a: 1, b: 2,         c: undefined, cfg: { b: 2, a: 1 } }
-      @eq     ( Ωnfat__16 = -> fn 1, 2, 3,    frz { b: 88, }  ), { a: 1, b: 2,         c: 3,         cfg: { b: 2, a: 1, c: 3 } }
-      @throws ( Ωnfat__17 = -> fn 1, 2, 3, 4, frz { b: 88, }  ), /expected up to 3 positional arguments, got 4/
+      @eq     ( Ωnfat___6 = -> fn                 {}          ), { a: undefined, b: undefined, c: undefined, cfg: {} }
+      @eq     ( Ωnfat___7 = -> fn 1,              {}          ), { a: 1,         b: undefined, c: undefined, cfg: { a: 1 } }
+      @eq     ( Ωnfat___8 = -> fn 1, 2,           {}          ), { a: 1,         b: 2,         c: undefined, cfg: { a: 1, b: 2 } }
+      @eq     ( Ωnfat___9 = -> fn 1, 2, 3,        {}          ), { a: 1,         b: 2,         c: 3,         cfg: { a: 1, b: 2, c: 3 } }
+      @throws ( Ωnfat__10 = -> fn 1, 2, 3, 4,     {}          ), /expected up to 3 positional arguments, got 4/
+      @eq     ( Ωnfat__11 = -> fn                 { b: 88, }  ), { a: undefined, b: 88,        c: undefined, cfg: { b: 88, } }
+      @eq     ( Ωnfat__12 = -> fn 1,              { b: 88, }  ), { a: 1,         b: 88,        c: undefined, cfg: { b: 88, a: 1 } }
+      @eq     ( Ωnfat__13 = -> fn 1, 2,           { b: 88, }  ), { a: 1,         b: 2,         c: undefined, cfg: { b: 2, a: 1 } }
+      @eq     ( Ωnfat__14 = -> fn 1, 2, 3,        { b: 88, }  ), { a: 1,         b: 2,         c: 3,         cfg: { b: 2, a: 1, c: 3 } }
+      @throws ( Ωnfat__15 = -> fn 1, 2, 3, 4,     { b: 88, }  ), /expected up to 3 positional arguments, got 4/
+      @eq     ( Ωnfat__16 = -> fn             frz { b: 88, }  ), { a: undefined, b: 88,        c: undefined, cfg: { b: 88, } }
+      @eq     ( Ωnfat__17 = -> fn 1,          frz { b: 88, }  ), { a: 1,         b: 88,        c: undefined, cfg: { b: 88, a: 1 } }
+      @eq     ( Ωnfat__18 = -> fn 1, 2,       frz { b: 88, }  ), { a: 1,         b: 2,         c: undefined, cfg: { b: 2, a: 1 } }
+      @eq     ( Ωnfat__19 = -> fn 1, 2, 3,    frz { b: 88, }  ), { a: 1,         b: 2,         c: 3,         cfg: { b: 2, a: 1, c: 3 } }
+      @throws ( Ωnfat__20 = -> fn 1, 2, 3, 4, frz { b: 88, }  ), /expected up to 3 positional arguments, got 4/
+      #.....................................................................................................
+      return null
+    #.......................................................................................................
+    do =>
+      fn = nfa ( cfg ) -> { cfg, }
+      frz = Object.freeze
+      #.....................................................................................................
+      @eq     ( Ωnfat__21 = -> fn()                           ), { cfg: {} }
+      @throws ( Ωnfat__22 = -> fn 1, 2, 3, 4                  ), /expected up to 0 positional arguments, got 4/
+      @eq     ( Ωnfat__23 = -> fn                 {}          ), { cfg: {} }
+      @throws ( Ωnfat__24 = -> fn 1, 2, 3, 4,     {}          ), /expected up to 0 positional arguments, got 4/
+      @eq     ( Ωnfat__25 = -> fn                 { b: 88, }  ), { cfg: { b: 88, } }
+      @throws ( Ωnfat__26 = -> fn 1, 2, 3, 4,     { b: 88, }  ), /expected up to 0 positional arguments, got 4/
+      @eq     ( Ωnfat__27 = -> fn             frz { b: 88, }  ), { cfg: { b: 88, } }
+      @throws ( Ωnfat__28 = -> fn 1, 2, 3, 4, frz { b: 88, }  ), /expected up to 0 positional arguments, got 4/
+      #.....................................................................................................
+      return null
+    #.......................................................................................................
+    do =>
+      fn = nfa () -> {}
+      frz = Object.freeze
+      #.....................................................................................................
+      @eq     ( Ωnfat__29 = -> fn()                           ), {}
+      @throws ( Ωnfat__30 = -> fn 1, 2, 3, 4                  ), /expected up to 0 positional arguments, got 4/
+      @throws ( Ωnfat__31 = -> fn                 {}          ), /expected up to 0 named arguments objects, got 1/
+      @throws ( Ωnfat__32 = -> fn 1, 2, 3, 4,     {}          ), /expected up to 0 named arguments objects, got 1/
+      @throws ( Ωnfat__33 = -> fn                 { b: 88, }  ), /expected up to 0 named arguments objects, got 1/
+      @throws ( Ωnfat__34 = -> fn 1, 2, 3, 4,     { b: 88, }  ), /expected up to 0 named arguments objects, got 1/
+      @throws ( Ωnfat__35 = -> fn             frz { b: 88, }  ), /expected up to 0 named arguments objects, got 1/
+      @throws ( Ωnfat__36 = -> fn 1, 2, 3, 4, frz { b: 88, }  ), /expected up to 0 named arguments objects, got 1/
       #.....................................................................................................
       return null
     #.......................................................................................................
@@ -69,15 +100,15 @@ GTNG                      = require '../../../apps/guy-test-NG'
 
         ) {};
       ```
-      @eq     ( Ωnfat__18 = -> get_signature ( a                        ) ->  ), { a: 'bare', }
-      @throws ( Ωnfat__19 = -> get_signature ( a = optional             ) ->  ), /not compliant/
-      @eq     ( Ωnfat__20 = -> get_signature empty_fn                         ), {}
-      @eq     ( Ωnfat__21 = -> get_signature                              ->  ), {}
-      @eq     ( Ωnfat__22 = -> get_signature ( a, b, c                        ) ->  ), { a: 'bare', b: 'bare', c: 'bare', }
+      @eq     ( Ωnfat__37 = -> get_signature ( a                        ) ->  ), { a: 'bare', }
+      @throws ( Ωnfat__38 = -> get_signature ( a = optional             ) ->  ), /not compliant/
+      @eq     ( Ωnfat__39 = -> get_signature empty_fn                         ), {}
+      @eq     ( Ωnfat__40 = -> get_signature                              ->  ), {}
+      @eq     ( Ωnfat__41 = -> get_signature ( a, b, c                        ) ->  ), { a: 'bare', b: 'bare', c: 'bare', }
       # ### TAINT limitation of CoffeeScript: signature runs up to soak, trailing paramters handled inside function body ###
-      # @eq     ( Ωnfat__23 = -> get_signature ( a, b..., c               ) ->  ), { a: 'bare', b: 'soak', }
-      @throws ( Ωnfat__23 = -> get_signature ( a, b..., c               ) ->  ), /not compliant/
-      @throws ( Ωnfat__24 = -> get_signature ( a, b = null              ) ->  ), /not compliant/
+      # @eq     ( Ωnfat__42 = -> get_signature ( a, b..., c               ) ->  ), { a: 'bare', b: 'soak', }
+      @throws ( Ωnfat__43 = -> get_signature ( a, b..., c               ) ->  ), /not compliant/
+      @throws ( Ωnfat__44 = -> get_signature ( a, b = null              ) ->  ), /not compliant/
     #.......................................................................................................
     return null
 
