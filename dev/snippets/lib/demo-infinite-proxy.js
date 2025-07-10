0beeb76a4715605b9de74f9bec86ba603b141cc6
loveencounterflow <flow@home>
2025-07-10 18:13:16 +0200

    update

--- dev/snippets/lib/demo-infinite-proxy.js
+++ dev/snippets/lib/demo-infinite-proxy.js
@@ -1,6 +1,6 @@
 (async function() {
   'use strict';
-  var A, GUY, alert, debug, demo_proxy, echo, f, gold, help, info, inspect, log, plain, praise, red, reverse, rpr, urge, warn, whisper, white, write;
+  var A, GUY, alert, debug, demo_proxy, echo, f, gold, help, info, inspect, log, nfa, plain, praise, red, reverse, rpr, urge, warn, whisper, white, write;
 
   //===========================================================================================================
   GUY = require('guy');
@@ -17,40 +17,43 @@
 
   A = require('ansis');
 
+  ({nfa} = require('../../../apps/normalize-function-arguments'));
+
   //===========================================================================================================
   demo_proxy = function() {
-    var base, new_infiniprox, new_infiniprox_B, p, stack;
+    /* Building the chain: */
+    var base, chain, new_infiniproxy, p, stack, template;
     stack = [];
-    new_infiniprox_B = function(base) {
-      var R;
-      R = new Proxy(base, {
+    //.........................................................................................................
+    template = {
+      base: null,
+      is_initial: false
+    };
+    //.........................................................................................................
+    new_infiniproxy = nfa({template}, function(base, is_initial, cfg) {
+      var R, proxy;
+      proxy = new Proxy(base, {
         get: function(target, key) {
           if ((typeof key) === 'symbol') {
             return target[key];
           }
-          // debug 'Ω___1', ( A.green.inverse.bold ' B ' ), { target, key, }, stack
-          stack.push(key);
-          return R;
-        }
-      });
-      return R;
-    };
-    new_infiniprox = function(base) {
-      var R;
-      R = new_infiniprox_B(base);
-      return new Proxy(base, {
-        get: function(target, key) {
-          if ((typeof key) === 'symbol') {
-            return target[key];
+          if (is_initial) {
+            stack.length = 0;
           }
-          // debug 'Ω___2', ( A.red.inverse.bold ' A ' ), { target, key, }, stack
-          stack.length = 0;
           stack.push(key);
           return R;
         }
       });
-      return R;
-    };
+      if (is_initial) {
+        R = new_infiniproxy({
+          base,
+          is_initial: false
+        });
+      } else {
+        R = proxy;
+      }
+      return proxy;
+    });
     //.........................................................................................................
     base = function(...P) {
       var R;
@@ -59,23 +62,29 @@
       stack.length = 0;
       return R;
     };
-    p = new_infiniprox(base);
+    p = new_infiniproxy(base, {
+      is_initial: true
+    });
     debug('Ω___4', p);
     debug('Ω___5', p.arc);
-    // debug 'Ω___6', p.arc.bo
-    // debug 'Ω___7', p.arc.bo.cy
     info('Ω___8', p.arc.bo.cy(8));
     //.........................................................................................................
     /* These calls will be `stack`ed but then get thrown away as soon as any property of `p` is used: */
-    debug('Ω___9', p.ooops);
-    debug('Ω__10', p.wat);
-    debug('Ω__11', p.nö);
-    debug('Ω__12', stack);
+    p.ooops;
+    debug('Ω___9', stack);
+    p.wat;
+    debug('Ω__10', stack);
+    p.nö;
+    debug('Ω__11', stack);
     info('Ω__13', p`some text`);
     debug('Ω__14', stack);
     //.........................................................................................................
     info('Ω__15', p.arc.bo.cy`some text`);
     info('Ω__16', p.arc.bo.cy.dean.blah`some text`);
+    chain = p.arc.bo.cy;
+    chain.dean.blah;
+    chain.and.then.some;
+    info('Ω__16', p("finally, a call"));
     return null;
   };
 
--- dev/snippets/src/demo-infinite-proxy.coffee
+++ dev/snippets/src/demo-infinite-proxy.coffee
@@ -25,52 +25,52 @@ GUY                       = require 'guy'
 { f }                     = require '../../../apps/effstring'
 write                     = ( p ) -> process.stdout.write p
 A                         = require 'ansis'
+{ nfa }                   = require '../../../apps/normalize-function-arguments'
 
 
 #===========================================================================================================
 demo_proxy = ->
   stack = []
-  new_infiniprox_B = ( base ) ->
-    R = new Proxy base,
+  #.........................................................................................................
+  template =
+    base:         null
+    is_initial:   false
+  #.........................................................................................................
+  new_infiniproxy = nfa { template, }, ( base, is_initial, cfg ) ->
+    proxy = new Proxy base,
       get: ( target, key ) ->
         return target[ key ] if ( typeof key ) is 'symbol'
-        # debug 'Ω___1', ( A.green.inverse.bold ' B ' ), { target, key, }, stack
+        stack.length = 0 if is_initial
         stack.push key
         return R
-    return R
-  new_infiniprox = ( base ) ->
-    R = new_infiniprox_B base
-    return new Proxy base,
-      get: ( target, key ) ->
-        return target[ key ] if ( typeof key ) is 'symbol'
-        # debug 'Ω___2', ( A.red.inverse.bold ' A ' ), { target, key, }, stack
-        stack.length = 0
-        stack.push key
-        return R
-    return R
+    if is_initial then  R = new_infiniproxy { base, is_initial: false, }
+    else                R = proxy
+    return proxy
   #.........................................................................................................
   base = ( P... ) ->
     # debug 'Ω___3', P
     R = "#{stack.join '.'}::#{rpr P}"
     stack.length = 0
     return R
-  p = new_infiniprox base
+  p = new_infiniproxy base, { is_initial: true, }
   debug 'Ω___4', p
   debug 'Ω___5', p.arc
-  # debug 'Ω___6', p.arc.bo
-  # debug 'Ω___7', p.arc.bo.cy
   info 'Ω___8', p.arc.bo.cy 8
   #.........................................................................................................
   ### These calls will be `stack`ed but then get thrown away as soon as any property of `p` is used: ###
-  debug 'Ω___9', p.ooops
-  debug 'Ω__10', p.wat
-  debug 'Ω__11', p.nö
-  debug 'Ω__12', stack
+  p.ooops;  debug 'Ω___9', stack
+  p.wat;    debug 'Ω__10', stack
+  p.nö;     debug 'Ω__11', stack
   info 'Ω__13', p"some text"
   debug 'Ω__14', stack
   #.........................................................................................................
   info 'Ω__15', p.arc.bo.cy"some text"
   info 'Ω__16', p.arc.bo.cy.dean.blah"some text"
+  ### Building the chain: ###
+  chain = p.arc.bo.cy
+  chain.dean.blah
+  chain.and.then.some
+  info 'Ω__16', p "finally, a call"
   return null
 
 
