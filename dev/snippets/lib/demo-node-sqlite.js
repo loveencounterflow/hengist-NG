3be1547986c3c5189ff1938e03cfdbfb2ef5cb69
loveencounterflow <flow@home>
2025-08-03 20:21:21 +0200

    update

--- dev/snippets/lib/demo-node-sqlite.js
+++ dev/snippets/lib/demo-node-sqlite.js
@@ -1,6 +1,6 @@
 (async function() {
   'use strict';
-  var GTNG, GUY, PATH, S, SFMODULES, SQL, Test, alert, blue, bold, debug, demo, echo, env_paths, f, get_rough_unicode_category, gold, green, grey, help, illegal_codepoint_patterns, info, inspect, log, mkdirp, nfa, plain, praise, red, reverse, rpr, urge, warn, whisper, white;
+  var GTNG, GUY, Node_sqlite, PATH, SFMODULES, SQL, SQLITE, Segment_width_db, Test, alert, blue, bold, debug, demo, echo, env_paths, f, get_rough_unicode_category, gold, green, grey, help, illegal_codepoint_patterns, info, inspect, log, mkdirp, nfa, plain, praise, red, reverse, rpr, urge, warn, whisper, white;
 
   //===========================================================================================================
   GUY = require('guy');
@@ -24,7 +24,7 @@
 
   env_paths = (require('env-paths')).default('demo-node-sqlite');
 
-  S = require('node:sqlite');
+  SQLITE = require('node:sqlite');
 
   PATH = require('node:path');
 
@@ -52,42 +52,106 @@
     return 'other';
   };
 
+  Node_sqlite = (function() {
+    //===========================================================================================================
+    class Node_sqlite {
+      //---------------------------------------------------------------------------------------------------------
+      constructor(db_path) {
+        var clasz, name, ref, sql;
+        this.db = new SQLITE.DatabaseSync(db_path);
+        clasz = this.constructor;
+        /* NOTE we can't just prepare all the stetments as they depend on DB objects existing or not existing,
+           as the case may be. Hence we prepare statements on-demand and cache them here as needed: */
+        this.statements = {};
+        ref = clasz.statements;
+        //.......................................................................................................
+        for (name in ref) {
+          sql = ref[name];
+          switch (true) {
+            case name.startsWith('create_table_'):
+              null;
+              break;
+            case name.startsWith('insert_'):
+              null;
+              break;
+            default:
+              throw new Error(`Ωnql___1 unable to parse statement name ${rpr(name)}`);
+          }
+        }
+        //   @[ name ] = @prepare sql
+        return void 0;
+      }
+
+      //---------------------------------------------------------------------------------------------------------
+      execute(sql) {
+        return this.db.exec(sql);
+      }
+
+      //---------------------------------------------------------------------------------------------------------
+      prepare(sql) {
+        return this.db.prepare(sql);
+      }
+
+    };
+
+    //---------------------------------------------------------------------------------------------------------
+    Node_sqlite.statements = {};
+
+    return Node_sqlite;
+
+  }).call(this);
+
+  Segment_width_db = (function() {
+    //===========================================================================================================
+    class Segment_width_db extends Node_sqlite {};
+
+    //---------------------------------------------------------------------------------------------------------
+    Segment_width_db.statements = {
+      //.......................................................................................................
+      create_table_segments: SQL`drop table if exists segments;
+create table segments (
+    segment_text      text    not null primary key,
+    segment_width     integer not null,
+    segment_length    integer not null,
+  constraint segment_width_eqgt_zero  check ( segment_width  >= 0 ),
+  constraint segment_length_eqgt_zero check ( segment_length >= 0 ) );`,
+      //.......................................................................................................
+      insert_segment: SQL`insert into segments  ( segment_text,   segment_width,  segment_length  )
+              values  ( $segment_text,  $segment_width, $segment_length )
+  on conflict ( segment_text ) do update
+              set     (                 segment_width,  segment_length  ) =
+                      ( excluded.segment_width, excluded.segment_length );`
+    };
+
+    return Segment_width_db;
+
+  }).call(this);
+
   //===========================================================================================================
   demo = () => {
-    var all_segments, chr, cid, cid_hex, db, db_path, i, idx, insert_segment, j, k, len, ref, segment, segment_length, segment_text, segment_width, some_segments, tmp_path, ucc, v, x;
+    var all_segments, cfg, chr, cid, cid_hex, db, db_path, i, idx, insert_segment, j, k, len, ref, segment_length, segment_text, segment_width, some_segments, some_segments_with_widths, tmp_path, ucc, v, x;
     for (k in env_paths) {
       v = env_paths[k];
-      debug('Ωnql___1', k, v);
+      debug('Ωnql___2', k, v);
     }
     tmp_path = env_paths.temp;
     db_path = PATH.join(tmp_path, 'chr-widths.sqlite');
-    debug('Ωnql___2', mkdirp.sync(tmp_path));
-    debug('Ωnql___3', db = new S.DatabaseSync(db_path));
+    debug('Ωnql___3', mkdirp.sync(tmp_path));
+    debug('Ωnql___4', db = new Segment_width_db(db_path));
     //.........................................................................................................
-    db.exec(SQL`drop table if exists segments;`);
-    //.........................................................................................................
-    db.exec(SQL`create table segments (
-  segment_text      text    not null primary key,
-  segment_width     integer not null,
-  segment_length    integer not null,
-  constraint segment_width_eqgt_zero  check ( segment_width >= 0  ),
-  constraint segment_length_eqgt_zero check ( segment_length >= 0 ) );`);
-    //.........................................................................................................
-    insert_segment = db.prepare(SQL`insert
-into segments ( segment_text, segment_width, segment_length )
-values ( $segment_text, $segment_width, $segment_length )
-on conflict ( segment_text ) do update
-  set ( segment_width, segment_length ) = ( excluded.segment_width, excluded.segment_length );`);
+    db.execute(SQL`drop table if exists segments;`);
+    db.execute(db.constructor.statements.create_table_segments);
+    insert_segment = db.prepare(db.constructor.statements.insert_segment);
     //.........................................................................................................
     all_segments = db.prepare(SQL`select * from segments order by segment_text;`);
     //.........................................................................................................
-    db.exec(SQL`begin transaction;`);
-    for (cid = i = 0x00_0000; i <= 255; cid = ++i) {
+    db.execute(SQL`begin transaction;`);
+    for (cid = i = 0x00_000c; i <= 80; cid = ++i) {
       chr = String.fromCodePoint(cid);
       cid_hex = `U+${(cid.toString(16)).padStart(4, '0')}`;
       chr = String.fromCodePoint(cid);
       ucc = get_rough_unicode_category(chr);
-      // debug 'Ωbbsfm___4', cid_hex, ( rpr chr ), ucc
+      // debug 'Ωbbsfm___5', cid_hex, ( rpr chr ), ucc
       segment_text = chr;
       segment_width = null;
       segment_length = null;
@@ -106,22 +170,36 @@ on conflict ( segment_text ) do update
       }
       insert_segment.run({segment_text, segment_width, segment_length});
     }
-    db.exec(SQL`commit;`);
+    db.execute(SQL`commit;`);
     for (x of all_segments.iterate()) {
       ({segment_text, segment_width, segment_length} = x);
-      info('Ωnql___5', rpr(segment_text), segment_width, segment_length);
+      info('Ωnql___6', rpr(segment_text), segment_width, segment_length);
     }
     //.........................................................................................................
     // some_segments = db.prepare SQL"""select * from segments where segment_text in ( $texts );"""
-    // debug 'Ωnql___6', some_segments.run { texts: [ 'a', 'b', ], }
+    // debug 'Ωnql___7', some_segments.run { texts: [ 'a', 'b', ], }
     some_segments = db.prepare(SQL`select * from segments where segment_text in (
 select value from json_each(?) );`);
     ref = some_segments.all(JSON.stringify(['a', 'b']));
     // some_segments.setReturnArrays true
     for (idx = j = 0, len = ref.length; j < len; idx = ++j) {
-      segment = ref[idx];
-      urge('Ωnql___7', rpr(segment), segment);
+      ({segment_text, segment_width, segment_length} = ref[idx]);
+      urge('Ωnql___8', idx, rpr(segment_text), segment_width, segment_length);
     }
+    //.........................................................................................................
+    cfg = {
+      deterministic: true,
+      varargs: false
+    };
+    db.db.function('width_from_text', cfg, function(text) {
+      return (Array.from(text)).length;
+    });
+    some_segments_with_widths = db.prepare(SQL`select
+  $text as my_text,
+  width_from_text( $text ) as width;`);
+    debug('Ωnql___9', some_segments_with_widths.all({
+      text: '765'
+    }));
     //.........................................................................................................
     return null;
   };
--- dev/snippets/src/demo-node-sqlite.coffee
+++ dev/snippets/src/demo-node-sqlite.coffee
@@ -32,7 +32,7 @@ GTNG                      = require '../../../apps/guy-test-NG'
 SFMODULES                 = require '../../../apps/bricabrac-single-file-modules'
 mkdirp                    = require 'mkdirp'
 env_paths                 = ( require 'env-paths' ).default 'demo-node-sqlite'
-S                         = require 'node:sqlite'
+SQLITE                    = require 'node:sqlite'
 PATH                      = require 'node:path'
 { SQL }                   = require '../../../apps/dbay'
 
@@ -52,40 +52,81 @@ get_rough_unicode_category = ( chr ) ->
     return name if pattern.test chr
   return 'other'
 
+#===========================================================================================================
+class Node_sqlite
+
+  #---------------------------------------------------------------------------------------------------------
+  @statements: {}
+
+  #---------------------------------------------------------------------------------------------------------
+  constructor: ( db_path ) ->
+    @db                 = new SQLITE.DatabaseSync db_path
+    clasz               = @constructor
+    ### NOTE we can't just prepare all the stetments as they depend on DB objects existing or not existing,
+    as the case may be. Hence we prepare statements on-demand and cache them here as needed: ###
+    @statements         = {}
+    #.......................................................................................................
+    for name, sql of clasz.statements
+      switch true
+        when name.startsWith 'create_table_'
+          null
+        when name.startsWith 'insert_'
+          null
+        else
+          throw new Error "Ωnql___1 unable to parse statement name #{rpr name}"
+    #   @[ name ] = @prepare sql
+    return undefined
+
+  #---------------------------------------------------------------------------------------------------------
+  execute: ( sql ) -> @db.exec sql
+
+  #---------------------------------------------------------------------------------------------------------
+  prepare: ( sql ) -> @db.prepare sql
+
+
+#===========================================================================================================
+class Segment_width_db extends Node_sqlite
+
+  #---------------------------------------------------------------------------------------------------------
+  @statements:
+    #.......................................................................................................
+    create_table_segments: SQL"""
+      drop table if exists segments;
+      create table segments (
+          segment_text      text    not null primary key,
+          segment_width     integer not null,
+          segment_length    integer not null,
+        constraint segment_width_eqgt_zero  check ( segment_width  >= 0 ),
+        constraint segment_length_eqgt_zero check ( segment_length >= 0 ) );"""
+    #.......................................................................................................
+    insert_segment: SQL"""
+      insert into segments  ( segment_text,   segment_width,  segment_length  )
+                    values  ( $segment_text,  $segment_width, $segment_length )
+        on conflict ( segment_text ) do update
+                    set     (                 segment_width,  segment_length  ) =
+                            ( excluded.segment_width, excluded.segment_length );"""
 
 #===========================================================================================================
 demo = =>
-  debug 'Ωnql___1', k, v for k, v of env_paths
+  debug 'Ωnql___2', k, v for k, v of env_paths
   tmp_path  = env_paths.temp
   db_path   = PATH.join tmp_path, 'chr-widths.sqlite'
-  debug 'Ωnql___2', mkdirp.sync tmp_path
-  debug 'Ωnql___3', db = new S.DatabaseSync db_path
+  debug 'Ωnql___3', mkdirp.sync tmp_path
+  debug 'Ωnql___4', db = new Segment_width_db db_path
   #.........................................................................................................
-  db.exec SQL"""drop table if exists segments;"""
-  #.........................................................................................................
-  db.exec SQL"""
-    create table segments (
-      segment_text      text    not null primary key,
-      segment_width     integer not null,
-      segment_length    integer not null,
-      constraint segment_width_eqgt_zero  check ( segment_width >= 0  ),
-      constraint segment_length_eqgt_zero check ( segment_length >= 0 ) );"""
-  #.........................................................................................................
-  insert_segment = db.prepare SQL"""insert
-    into segments ( segment_text, segment_width, segment_length )
-    values ( $segment_text, $segment_width, $segment_length )
-    on conflict ( segment_text ) do update
-      set ( segment_width, segment_length ) = ( excluded.segment_width, excluded.segment_length );"""
+  db.execute SQL"""drop table if exists segments;"""
+  db.execute db.constructor.statements.create_table_segments
+  insert_segment = db.prepare db.constructor.statements.insert_segment
   #.........................................................................................................
   all_segments = db.prepare SQL"""select * from segments order by segment_text;"""
   #.........................................................................................................
-  db.exec SQL"""begin transaction;"""
-  for cid in [ 0x00_0000 .. 0x00_00ff ]
+  db.execute SQL"""begin transaction;"""
+  for cid in [ 0x00_000c .. 0x00_0050 ]
     chr     = String.fromCodePoint cid
     cid_hex = "U+#{( cid.toString 16 ).padStart 4, '0'}"
     chr     = String.fromCodePoint cid
     ucc     = get_rough_unicode_category chr
-    # debug 'Ωbbsfm___4', cid_hex, ( rpr chr ), ucc
+    # debug 'Ωbbsfm___5', cid_hex, ( rpr chr ), ucc
     segment_text    = chr
     segment_width   = null
     segment_length  = null
@@ -100,17 +141,26 @@ demo = =>
         segment_width   = 1 ### TAINT run wc --max-line-length ###
         segment_length  = 1
     insert_segment.run { segment_text, segment_width, segment_length, }
-  db.exec SQL"""commit;"""
+  db.execute SQL"""commit;"""
   for { segment_text, segment_width, segment_length, } from all_segments.iterate()
-    info 'Ωnql___5', ( rpr segment_text ), segment_width, segment_length
+    info 'Ωnql___6', ( rpr segment_text ), segment_width, segment_length
   #.........................................................................................................
   # some_segments = db.prepare SQL"""select * from segments where segment_text in ( $texts );"""
-  # debug 'Ωnql___6', some_segments.run { texts: [ 'a', 'b', ], }
+  # debug 'Ωnql___7', some_segments.run { texts: [ 'a', 'b', ], }
   some_segments = db.prepare SQL"""select * from segments where segment_text in (
     select value from json_each(?) );"""
   # some_segments.setReturnArrays true
-  for segment, idx in some_segments.all ( JSON.stringify [ 'a', 'b', ] )
-    urge 'Ωnql___7', ( rpr segment ), segment
+  for { segment_text, segment_width, segment_length, }, idx in some_segments.all ( JSON.stringify [ 'a', 'b', ] )
+    urge 'Ωnql___8', idx, ( rpr segment_text ), segment_width, segment_length
+  #.........................................................................................................
+  cfg = { deterministic: true, varargs: false, }
+  db.db.function 'width_from_text', cfg, ( text ) ->
+    return ( Array.from text ).length
+  some_segments_with_widths = db.prepare SQL"""
+    select
+      $text as my_text,
+      width_from_text( $text ) as width;"""
+  debug 'Ωnql___9', some_segments_with_widths.all { text: '765', }
   #.........................................................................................................
   return null
 
